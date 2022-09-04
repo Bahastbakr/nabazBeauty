62769b0e0fdb8d35eaff5b5702a29e5c833912d6
Bahast bakr <bahastbaker@gmail.com>
2022-09-04 16:09:41 +0300

    href="{{ route('indexProductClient') }}"

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,9 +14,10 @@
                     </h1>
                     <h1 data-aos="fade-left" data-aos-duration="1200"
                         class="text-6xl md:text-9xl magiesta nature-green">Be You..</h1>
-                    <button data-aos="fade-up" data-aos-duration="500" type="button"
+                    <a href="{{ route('indexProductClient') }}" data-aos="fade-up" data-aos-duration="500"
+                        type="button"
                         class="text-white md:mt-10 bg-nature-green  hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300  rounded-full text-sm px-5 py-2.5 text-center md:text-4xl mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">More
-                        info</button>
+                        info</a>
 
                 </div>
 
